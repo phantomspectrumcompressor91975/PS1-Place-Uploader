c2de058ad8f161ae1fe419b0de87ae8dbac9a60f
Phantom <188271330+phantomspectrumcompressor91975@users.noreply.github.com>
2025-12-12 00:41:04 -0700

    i named it bad

--- script.ps1
+++ script.ps1
@@ -1,8 +1,8 @@
-if (Test-Path -Path data.txt) {
+if (Test-Path -Path input.txt) {
     $rbxlFile = Get-ChildItem -Filter *.rbxl -Recurse -File | Select-Object -First 1
 
     if ($rbxlFile) {
-        Get-Content data.txt | Foreach-Object {
+        Get-Content input.txt | Foreach-Object {
             $var = $_.Split('=')
             New-Variable -Name $var[0] -Value $var[1]
          }
