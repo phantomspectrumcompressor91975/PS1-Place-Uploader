6574bd7ae347310968a0e539c80202becac3c369
Phantom <188271330+phantomspectrumcompressor91975@users.noreply.github.com>
2025-12-12 00:27:13 -0700

    when

--- script.ps1
+++ script.ps1
@@ -1,1 +1,5 @@
-Get-ChildItem -Filter *.rbxl -Recurse -File | Select-Object -First 1
+$rbxlFile = Get-ChildItem -Filter *.rbxl -Recurse -File | Select-Object -First 1
+
+if ($rbxlFile) {
+    $rbxlFile
+}
