4e034e5a76a22279601802415c4ef7a4a99b064f
Phantom <188271330+phantomspectrumcompressor91975@users.noreply.github.com>
2025-12-12 00:36:03 -0700

    probably a better way to do this but idc

--- script.ps1
+++ script.ps1
@@ -1,7 +1,14 @@
-$rbxlFile = Get-ChildItem -Filter *.rbxl -Recurse -File | Select-Object -First 1
+if (Test-Path -Path data.txt) {
+    $rbxlFile = Get-ChildItem -Filter *.rbxl -Recurse -File | Select-Object -First 1
 
-if ($rbxlFile -and (Test-Path -Path data.txt)) {
-    $textContent = Get-Content data.txt
+    if ($rbxlFile) {
+        Get-Content data.txt | Foreach-Object {
+            $var = $_.Split('=')
+            New-Variable -Name $var[0] -Value $var[1]
+         }
 
-    $rbxlFile
+        $rbxlFile
+        $username
+        $password
+    }
 }
